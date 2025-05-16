c10ba7afe550968e8fae2457232a3220d1cbc267
NAGARAJU YANAGANDULA <raju.rajudba7@gmail.com>
2025-05-16 23:32:57 +0530

    added star and hash

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ def velocity(s:'in miles',t:'in hours') -> 'mph':
     return s/t
 print(velocity(30,15))
 print(velocity.__annotations__)
-print("************************")
-print("########################")
+print("*************************")
+print("#########################")
